04eac416fa2e4aab2916f7a82765a7bc724d7249
Robobyte Studios <nk.kavinkrishna@gmail.com>
2025-08-18 13:37:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4810,7 +4810,7 @@ if st.user.is_logged_in:
 
 else:
     st.set_page_config(layout="wide", page_title="SepSim", page_icon = "logo.png")
-    col1, col2, col3 = st.columns([5, 3, 5])  # Adjust the ratios as needed
+    col1, col2, col3 = st.columns([3, 3, 3])  # Adjust the ratios as needed
     
     with col2:
             st.markdown("---")        
@@ -4824,7 +4824,7 @@ else:
                     )   
                 # Wrap your checkbox label with tooltip
             st.image("Sep1.png", caption="",  use_container_width=True)
-            st.markdown("---")
+            
 
     col1 , col2 , col3 = st.columns([5,1,5])
     with col2:
@@ -4847,6 +4847,7 @@ else:
             st.stop()
             
     
+
 
 
 
