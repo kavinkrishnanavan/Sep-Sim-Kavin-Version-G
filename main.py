87cb81c2bc5f4e144fb27b42c9eb6305a1027489
Robobyte Studios <nk.kavinkrishna@gmail.com>
2025-08-18 13:06:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4821,7 +4821,7 @@ else:
 
    # with col1:
 
-    col1 , col2 , col3 = st.columns([5,3,5])
+    col1 , col2 , col3 = st.columns([3,5,3])
 
         
     
@@ -4834,6 +4834,7 @@ else:
             st.stop()
             
     
+
 
 
 
